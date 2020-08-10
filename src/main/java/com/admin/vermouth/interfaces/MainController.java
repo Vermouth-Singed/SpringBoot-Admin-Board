e89bdd427dc997b36c9862e9c9490702a8e349e3
Admin <qhddusl2004@naver.com>
2020-08-11 02:25:38 +0900

    commit frontend design...

--- src/main/java/com/admin/vermouth/interfaces/MainController.java
+++ src/main/java/com/admin/vermouth/interfaces/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/main")
+    @GetMapping("/dist")
     public String main(){return "/dist/index.html";}
 
     @GetMapping("/error")
