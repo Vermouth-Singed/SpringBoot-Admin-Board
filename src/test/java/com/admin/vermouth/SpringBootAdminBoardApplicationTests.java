a83ecb1065cf95318b756ce9b2a683a09bf4dbf9
Admin <qhddusl2004@naver.com>
2020-10-25 00:22:23 +0900

    modify AWS setting errors

--- src/test/java/com/admin/vermouth/SpringBootAdminBoardApplicationTests.java
+++ src/test/java/com/admin/vermouth/SpringBootAdminBoardApplicationTests.java
@@ -1,13 +1,7 @@
 package com.admin.vermouth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootAdminBoardApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
